22a87421d7b82ee00084dc6a3b67778d2f603bc4
davidlegarre <davidls920@gmail.com>
2026-08-15 21:00:51 +0200

    show cards changes

--- Frontend/src/components/show/ShowCard.tsx
+++ Frontend/src/components/show/ShowCard.tsx
@@ -1,15 +1,42 @@
 import "./ShowCard.css";
+
+const circleSVG = (
+  <span className="genre-entry">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+    </svg>
+  </span>
+);
+
+var genres = ["Drama", "Horror", "Humour"];
+
 function ShowCard() {
   return (
     <div className="show-card">
-      <img src="/vertical_placeholder.png" />
-      <h3 className="show-card__title">Title</h3>
-      <p className="show-card__description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
-        earum aperiam, aliquid cupiditate quaerat rem rerum beatae praesentium
-        placeat expedita et. Voluptas voluptatum, dolore fugit error amet odio
-        reprehenderit quibusdam!
-      </p>
+      <div className="show-card__poster">
+        <img src="/vertical_placeholder.png" />
+      </div>
+      <div className="text-box-show-card">
+        <h2 className="show-card__title">Title</h2>
+        <div className="genres-list">
+          {genres.flatMap((g, i) =>
+            i ? [circleSVG, <small>{g}</small>] : [<small>{g}</small>],
+          )}
+        </div>
+        <p className="show-card__description">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+          earum aperiam, aliquid cupiditate quaerat rem rerum beatae praesentium
+          placeat expedita et. Voluptas voluptatum, dolore fugit error amet odio
+          reprehenderit quibusdam!
+        </p>
+      </div>
     </div>
   );
 }
